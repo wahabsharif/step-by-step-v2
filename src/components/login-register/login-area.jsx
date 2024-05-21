803fbb53875087d499cd8dad90873cc03dcc0f17
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-21 18:16:23 +0500

    Login Page

--- src/components/login-register/login-area.jsx
+++ src/components/login-register/login-area.jsx
@@ -6,7 +6,6 @@ import LoginShapes from "./login-shapes";
 import GoogleSignUp from "./google-sign-up";
 
 const LoginArea = () => {
-
   return (
     <>
       <section className="tp-login-area pb-140 p-relative z-index-1 fix">
@@ -16,7 +15,7 @@ const LoginArea = () => {
             <div className="col-xl-6 col-lg-8">
               <div className="tp-login-wrapper">
                 <div className="tp-login-top text-center mb-30">
-                  <h3 className="tp-login-title">Login to Shofy.</h3>
+                  <h3 className="tp-login-title">Login to Step By Step.</h3>
                   <p>
                     Don’t have an account?{" "}
                     <span>
@@ -27,7 +26,7 @@ const LoginArea = () => {
                 <div className="tp-login-option">
                   <div className="tp-login-social mb-10 d-flex flex-wrap align-items-center justify-content-center">
                     <div className="tp-login-option-item has-google">
-                      <GoogleSignUp/>
+                      <GoogleSignUp />
                     </div>
                   </div>
                   <div className="tp-login-mail text-center mb-40">
