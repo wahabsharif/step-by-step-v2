e6c4d93ca0c337958b77cebdd836df7f034ceb1c
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-21 18:13:37 +0500

    Footer Updated

--- src/data/social-data.js
+++ src/data/social-data.js
@@ -1,29 +1,34 @@
-
 const social_data = [
   {
-    id:1,
-    link:'https://www.facebook.com/hamed.y.hasan0',
-    icon:'fa-brands fa-facebook-f',
-    title:'Facebook'
+    id: 1,
+    link: "#",
+    icon: "fa-brands fa-facebook-f",
+    title: "Facebook",
   },
   {
-    id:2,
-    link:'https://twitter.com/HamedHasan75',
-    icon:'fa-brands fa-twitter',
-    title:'Twitter'
+    id: 2,
+    link: "#",
+    icon: "fa-brands fa-twitter",
+    title: "Twitter",
   },
   {
-    id:3,
-    link:'nkedin.com/in/hamed-hasan/',
-    icon:'fa-brands fa-linkedin-in',
-    title:'Linkedin'
+    id: 3,
+    link: "#",
+    icon: "fa-brands fa-linkedin-in",
+    title: "Linkedin",
   },
   {
-    id:4,
-    link:'https://vimeo.com/',
-    icon:'fa-brands fa-vimeo-v',
-    title:'Vimeo'
+    id: 4,
+    link: "#",
+    icon: "fa-brands fa-vimeo-v",
+    title: "Vimeo",
   },
-]
+  {
+    id: 5,
+    link: "#",
+    icon: "fa-brands fa-weixin",
+    title: "WeChat",
+  },
+];
 
 export default social_data;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,15 @@
-import FeatureArea from "@/components/home/feature-area";
-import HomeBanner from "@/components/home/home-banner";
-import ProductArea from "@/components/products/services/product-area";
-import SEO from "@/components/seo";
-import Header from "@/layout/headers/header-2";
-import Wrapper from "@/layout/wrapper";
-import ProductSmArea from "@/components/products/services/product-sm-area";
-import NewArrivals from "@/components/products/services/new-arrivals";
-import BlogArea from "@/components/blog/electronic/blog-area";
-import InstagramArea from "@/components/instagram/instagram-area";
+import ServicesCategories from "@/components/categories/services-categories";
 import CtaArea from "@/components/cta/cta-area";
 import Areas from "@/components/home/areas";
-import Footer from "@/layout/footers/footer";
+import FeatureArea from "@/components/home/feature-area";
+import HomeBanner from "@/components/home/home-banner";
+import NewArrivals from "@/components/products/services/new-arrivals";
+import ProductArea from "@/components/products/services/product-area";
 import ProductGadgetArea from "@/components/products/services/product-gadget-area";
-import ServicesCategories from "@/components/categories/services-categories";
+import SEO from "@/components/seo";
+import Footer from "@/layout/footers/footer";
+import Header from "@/layout/headers/header-2";
+import Wrapper from "@/layout/wrapper";
 
 export default function Home() {
   return (
