a96d64b9b2ddd25d73e7434041854dc68ec27ce8
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-24 14:56:37 +0500

    config

--- next.config.js
+++ next.config.js
@@ -7,14 +7,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/about",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
-};
