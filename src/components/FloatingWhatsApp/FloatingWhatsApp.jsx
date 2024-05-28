ef41bf9cfb432aa4c16c2b53c85f72801c4ae51c
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-28 14:42:20 +0500

    Floating Whats App

--- src/components/FloatingWhatsApp/FloatingWhatsApp.jsx
+++ src/components/FloatingWhatsApp/FloatingWhatsApp.jsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const FloatingWhatsApp = () => {
   return (
     <a
-      href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202."
+      href="https://api.whatsapp.com/send?phone=971542474720&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202."
       className="float"
       target="_blank"
       rel="noopener noreferrer"
